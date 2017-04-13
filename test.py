3ce804fe3b3256d0734e4d1db39546f68439ef24
Eyal <eyalgutf@gmail.com>
2017-04-13 23:30:16 +0200

    Changed some stuff

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from multiprocessing import Pool
 
-def execMy(a):
-    print(a)
+def execMy(b):
+    print(b)
 if __name__ == '__main__':
     p=Pool(1)
     args=[[1] ,[2] ,[3]]
@@ -9,3 +9,6 @@ if __name__ == '__main__':
 	
 	
 	#My new comment
+	
+	
+	
